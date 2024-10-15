f414c7db88f0fb5861817e74f18f4970c24faa8c
mattisafur <46404761+mattisafur@users.noreply.github.com>
2024-10-15 16:21:58 +0300

    set database URL to taken from env vars

--- database.py
+++ database.py
@@ -1,9 +1,24 @@
+from os import environ
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
 
-DATABASE_URL: str = "postgresql://postgres:postgres@127.0.0.1:5432/auth"  # TODO get URI from env variable
+def get_required_env_variable(name: str) -> str:
+    variable = environ.get(name)
+    if variable is None:
+        raise EnvironmentError(f"Environment variable {name} is not set")
+    return variable
+
+
+DATABASE_HOST = get_required_env_variable("DATABASE_URL")
+DATABASE_PORT = get_required_env_variable("DATABASE_PORT")
+DATABASE_USERNAME = get_required_env_variable("DATABASE_USERNAME")
+DATABASE_PASSWORD = get_required_env_variable("DATABASE_PASSWORD")
+DATABASE_NAME = get_required_env_variable("DATABASE_NAME")
+
+DATABASE_URL: str = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
 if not database_exists(DATABASE_URL):
     create_database(DATABASE_URL)
 
